52c42fedb9a92b4080b8df3242c7ec905fbef793
Matty Muir <matty.muir@btinternet.com>
2026-05-09 16:22:33 +0100

    Switch to my CMake template.

---
+++ examples/example.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <format>
+
+#include <kissatlib.h>
+
+int main()
+{
+	
+}
---
+++ tests/tests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <charconv>
+#include <format>
+
+#include <kissatlib.h>
+
+#define ERROR(msg) { std::cerr << msg << '\n'; return 1; }
+
+int main(int argc, char** argv)
+{
+
+}
